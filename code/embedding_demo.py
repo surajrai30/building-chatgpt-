f89905f30ad4fb70e6951c7e0fab6c01fbaded58
surajrai30 <surajroyyi1@gmail.com>
2026-06-24 22:08:51 +0530

    Update embedding_demo.py

--- code/embedding_demo.py
+++ code/embedding_demo.py
@@ -1,1 +1,23 @@
+import numpy as np
 
+vocabulary = {
+    "i": 0,
+    "love": 1,
+    "ai": 2,
+    "learning": 3
+}
+
+embedding_dimension = 4
+
+embedding_matrix = np.random.rand(
+    len(vocabulary),
+    embedding_dimension
+)
+
+print("Embedding Matrix:\n")
+print(embedding_matrix)
+
+word = "ai"
+
+print("\nEmbedding for", word)
+print(embedding_matrix[vocabulary[word]])
